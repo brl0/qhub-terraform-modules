4192d3d93aae89464100c63baa4525b8af9db546
Brian Larsen <B_R_L@hotmail.com>
2021-02-19 11:33:58 -0600

    Move values file

--- modules/kind/ingress/main.tf
+++ modules/kind/ingress/main.tf
@@ -11,7 +11,7 @@ resource "helm_release" "ingress" {
   dependency_update = true
 
   values = concat([
-    file("${path.module}/chart/charts/values.yaml"),
+    file("${path.module}/values.yaml"),
     jsonencode({
       "cert-manager" = {
         affinity = local.affinity
