bbc4643bfdf9135a627afdfea04ffdd07b31f7fd
Brian Larsen <B_R_L@hotmail.com>
2021-02-19 12:42:49 -0600

    Use helm charts directly rather than as deps

--- modules/kind/ingress/main.tf
+++ modules/kind/ingress/main.tf
@@ -4,30 +4,52 @@ resource "null_resource" "dependency_getter" {
   }
 }
 
-resource "helm_release" "ingress" {
-  name      = "ingress"
-  namespace = var.namespace
-  chart = "${path.module}/chart"
-  dependency_update = true
-  force_update = true
-  recreate_pods = true
-  replace = true
+data "helm_repository" "jetstack" {
+  name = "jetstack"
+  url  = "https://charts.jetstack.io"
+}
 
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  namespace  = var.namespace
+  repository = data.helm_repository.jetstack.metadata[0].name
+  chart      = "cert-manager"
   values = concat([
-    file("${path.module}/values.yaml"),
+    file("${path.module}/cert-manager.yaml"),
     jsonencode({
       "cert-manager" = {
         affinity = local.affinity
-
         cainjector = {
           affinity = local.affinity
         }
-
         webhook = {
           affinity = local.affinity
         }
       }
+    }),
+  ])
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+  depends_on = [
+    null_resource.dependency_getter,
+  ]
+}
 
+data "helm_repository" "ingress-nginx" {
+  name = "ingress-nginx"
+  url  = "https://kubernetes.github.io/ingress-nginx"
+}
+
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  namespace  = var.namespace
+  repository = data.helm_repository.ingress-nginx.metadata[0].name
+  chart      = "ingress-nginx"
+  values = concat([
+    file("${path.module}/ingress-nginx.yaml"),
+    jsonencode({
       "nginx-ingress" = {
         controller = {
           affinity = local.affinity
@@ -35,31 +57,32 @@ resource "helm_release" "ingress" {
             timeoutSeconds = 20
           }
         }
-
         defaultBackend = {
           affinity = local.affinity
         }
       }
     }),
-  ], var.overrides)
-
+  ])
   depends_on = [
-    null_resource.dependency_getter
+    helm_release.cert-manager,
   ]
 }
 
-data "kubernetes_service" "ingress" {
-  depends_on = [helm_release.ingress]
+resource "time_sleep" "wait_30_seconds" {
+  depends_on      = [helm_release.ingress-nginx]
+  create_duration = "30s"
+}
 
-  metadata {
-    name      = "ingress-nginx-ingress-controller"
-    namespace = var.namespace
-  }
+resource "helm_release" "clusterissuer" {
+  name       = "clusterissuer"
+  chart      = "${path.module}/chart"
+  values     = [file("${path.module}/clusterissuer.yaml")]
+  depends_on = [time_sleep.wait_30_seconds]
 }
 
 resource "null_resource" "dependency_setter" {
   depends_on = [
-    helm_release.ingress
+    helm_release.clusterissuer,
     # List resource(s) that will be constructed last within the module.
   ]
 }
--- modules/kind/ingress/variables.tf
+++ modules/kind/ingress/variables.tf
@@ -11,12 +11,6 @@ variable "node-group" {
   })
 }
 
-variable "overrides" {
-  description = "Overrides for values.yaml in helm configuration"
-  type        = list(string)
-  default     = []
-}
-
 variable "dependencies" {
   description = "A list of module dependencies to be injected in the module"
   type        = list(any)
