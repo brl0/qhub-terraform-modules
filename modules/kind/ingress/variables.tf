b9d79cbaf9c15bfd5a11c9a258549598eaaf707c
Brian Larsen <B_R_L@hotmail.com>
2021-02-28 15:27:00 -0600

    Add vars

--- modules/kind/ingress/variables.tf
+++ modules/kind/ingress/variables.tf
@@ -1,3 +1,16 @@
+variable "namespace" {
+  description = "Namespace to deploy kubernetes ingress"
+  type        = string
+}
+
+variable "node-group" {
+  description = "Node group to associate ingress deployment"
+  type = object({
+    key   = string
+    value = string
+  })
+}
+
 variable "dependencies" {
   description = "A list of module dependencies to be injected in the module"
   type        = list(any)
